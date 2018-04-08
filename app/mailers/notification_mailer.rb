46c093b966973c5c4e8b3cdce77d97e70f9d26b1
banicoken <banicoken@gmail.com>
2018-04-08 01:45:19 +0000

    attempt to fix mailer

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -4,6 +4,7 @@ class NotificationMailer < ApplicationMailer
   def comment_added(comment)
     @place = comment.place
     @place_owner = @place.user
+    @comment = comment.message
     mail(to: @place_owner.email,
       subject: "A comment has been added to #{@place.name}")
   end
